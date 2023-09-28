e3f3af9e8641d491739eba5345f3e93fb1d5887d
Eric Russell <Erictim.Russell@live.com>
2023-09-28 10:04:21 -0600

    init push/tailwindcss

---
+++ src/views/AboutView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    Hello from about view
+  </div>
+</template>
+
+<script setup>
+
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1 class="text-3xl font-bold underline text-red-700">
+    Hello world!
+  </h1>
+</template>
+
+<script setup>
+
+</script>
+
